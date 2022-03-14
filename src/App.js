0ac190918d2162e8e87a97e435026aca1fc1fe96
Zulma Rocio <zrmartinezg@gmail.com>
2022-03-14 17:29:44 -0500

    Add delete functionality

--- src/App.js
+++ src/App.js
@@ -6,11 +6,19 @@ import FeedbackData from "./data/FeedbackData";
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData)
+
+  const deleteFeedback = (id) => {
+    if(window.confirm("Are you sure?")){
+      setFeedback(feedback.filter((item)=> item.id !== id ))
+    }
+  }
   return (
     <div className="App">
       <Header />
       <div className="container">
-        <FeedbackList feedback={feedback} />
+        <FeedbackList
+         feedback={feedback}
+         handleDelete={deleteFeedback} />
       </div>
     </div>
   );
